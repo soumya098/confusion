56f70781feea6bb7c9f00e9ac9772ec7981adf6a
soumya098 <soumyarj904@gmail.com>
2021-02-14 21:53:48 +0530

    Updates

---
+++ precache-manifest.6326046aec9174e08f3e8a541691b489.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e02f643e08a808b6f7415158de8eb0",
+    "url": "/confusion/index.html"
+  },
+  {
+    "revision": "60e09f1ecb663d7ac2e1",
+    "url": "/confusion/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "abf697ab4fab53d6ea09",
+    "url": "/confusion/static/css/main.26c23897.chunk.css"
+  },
+  {
+    "revision": "60e09f1ecb663d7ac2e1",
+    "url": "/confusion/static/js/2.c6c96c2f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/confusion/static/js/2.c6c96c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf697ab4fab53d6ea09",
+    "url": "/confusion/static/js/main.d68fbc27.chunk.js"
+  },
+  {
+    "revision": "e33b51f87c4ccb4dbd67",
+    "url": "/confusion/static/js/runtime-main.29c20dfb.js"
+  }
+]);
